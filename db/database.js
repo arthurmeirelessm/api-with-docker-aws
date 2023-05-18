99ca81c67efe3115a5afa1af89e970fd5bba2f3d
Arthur Mendes <arthur.mendes@compasso.com.br>
2023-05-18 17:06:53 -0300

    Add postgres connection and test - ERROR

--- db/database.js
+++ db/database.js
@@ -1,7 +1,14 @@
-const { Pool } = require('pg')
-require('dotenv').config()
- 
+const { Pool } = require('pg');
+require('dotenv').config();
+
 
 const pool = new Pool({
-     connectionString: process.env.CONNECTION_STRING    
-})
+     user: process.env.USER,
+     host: process.env.HOST,
+     database: process.env.DATABASE,
+     password: process.env.PASSWORD,
+     port: process.env.PORT, // porta padrão do PostgreSQL
+   });
+
+
+   module.exports = pool
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-const express = require('express');
-const server = express()
+const express = require("express");
+const pool = require("../db/database");
+const server = express();
+require("dotenv").config();
 
-const port = '3000'
+const port = "3000";
 
-server.get('/c', (req, res) => {
+async function getUsers(){
+  try {
+    const client = pool.connect();
+    const query = pool.query("SELECT * FROM carros");
+    return query;
+  } catch (e) {
+    console.log(e)
+  }
+};
 
-    return res.json({
-        "message":"Deu bom!",
-        "status":"200"
-     })
+server.get("/c", (req, res) => {
+  return getUsers();
 });
 
 server.listen(port);
